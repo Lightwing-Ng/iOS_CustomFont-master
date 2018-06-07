724c594af1f1b8101638177408e2dd2e5f9513d0
Lightwing-Ng <rodney_ng@icloud.com>
2018-06-07 19:18:52 +0800

    -

--- CustomFont/ViewController.swift
+++ CustomFont/ViewController.swift
@@ -8,18 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+@IBDesignable
+class DesignableViewController: UIViewController {
+     @IBInspectable var LightStatusBar: Bool = false
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            if LightStatusBar {
+                return UIStatusBarStyle.lightContent
+            } else {
+                return UIStatusBarStyle.default
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+
+
+class ViewController: DesignableViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var data = [
+        "30 Days Swift",
+        "顶配辉腾 Top Gear Phaeton",
+        "婊学测试 Bitchsim",
+        "钟晓迪",
+        "佩子临",
+        "唐宇恒",
+    ]
+    
+    var fontNames = [
+        "MFTongXin_Noncommercial-Regular",
+        "MFJinHei_Noncommercial-Regular",
+        "MFZhiHei_Noncommercial-Regular",
+        "Zapfino",
+        "Gaspar Regular"
+    ]
+    
+    var fontRowIndex = 0
+    
+    @IBOutlet weak var changeFontButton: UIButton!
+    @IBOutlet weak var fontTableView: UITableView!
+    @IBAction func changeFontDidTouch(_ sender: AnyObject) {
+        fontRowIndex = (fontRowIndex + 1) % 5
+        print(fontNames[fontRowIndex])
+        fontTableView.reloadData()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fontTableView.dataSource = self
+        fontTableView.delegate = self
+        changeFontButton.layer.cornerRadius = 60
+        changeFontButton.layer.borderWidth = 1
+        changeFontButton.layer.borderColor = UIColor(red:15/255, green:136/255, blue:235/255, alpha: 1).cgColor
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 35
+    }
+
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = fontTableView.dequeueReusableCell(withIdentifier: "FontCell", for: indexPath)
+        let text = data[indexPath.row]
+        cell.textLabel?.text = text
+        cell.textLabel?.textColor = UIColor(red: 15/255, green: 136/255, blue: 235/255, alpha: 1.0)
+        cell.textLabel?.font = UIFont(name:self.fontNames[fontRowIndex], size: 32)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+}

f2ecbff17567bde8d7704d2504352a02ab6a60e0
Lightwing-Ng <rodney_ng@icloud.com>
2018-06-07 12:07:22 +0800

    Initial Commit

---
+++ CustomFont/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomFont
+//
+//  Created by Lightwing Ng on 2018/6/7.
+//  Copyright © 2018 Lightwing Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
